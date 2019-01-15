c1ea7099c9ae926f1f9abc13125736239a22543b
Mārtiņš Buhanovskis <martins.buhanovskis@gmail.com>
2019-01-15 14:44:39 +0200

    added comments

--- cypress/integration/googleSearch/googleSearchCypress.spec.js
+++ cypress/integration/googleSearch/googleSearchCypress.spec.js
@@ -28,9 +28,9 @@ describe('Google search Cypress.io', function() {
         })
         it('Should input Cypress.io into input field and click on cypress.io',() => {
             cy.get('.gLFyf').type('Cypress.io')
-            cy.wait(1000)
+            //cy.wait(1000)
             cy.get('li.sbct:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > b:nth-child(1)').click()
-            cy.wait(1000)
+            //cy.wait(1000)
         })
         it('Should check search result and go to cypress page',() => {
             cy.contains('body')
@@ -39,13 +39,15 @@ describe('Google search Cypress.io', function() {
             .should('have.text', 'Cypress.io: JavaScript End to End Testing Framework')
             //cy.get('#rso > div:nth-child(1) > div > div > div > div > div.r > a:nth-child(1) > h3').click()
         })
-        //it('Check cypress.io page', () => {
-            // cypress do not allow superdomain change !
+        it('Check cypress.io page', () => {
+            // cypress do not allow superdomain change ! --> set cheome web security to false
 
-            //cy.get('#rso > div:nth-child(1) > div > div > div > div > div.r > a:nth-child(1) > h3').click()
+            cy.get('#rso > div:nth-child(1) > div > div > div > div > div.r > a:nth-child(1) > h3').click().end()
+            //cy.wait(1000)
             //cy.location(cy.url(),{timeout: 60000})
             //cy.wait(1000)
-            //cy.url().should('eq', 'https://www.cypress.io/')
-        //})
+            cy.url().should('eq', 'https://www.cypress.io/')
+            //cy.wait(1000)
+        })
     })
 })
